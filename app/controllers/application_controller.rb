e68ed8f8dcbb501afcff11cc3c8bdaad4936a2d7
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-28 22:29:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
      if params[:username] == "" || params[:password] == ""
       redirect '/failure'
     else  
-      erb :account
+      redirect '/login'
     end
   end
 
